f131ad6e3eb901d27233e8afd8f9f06112ff3848
PereMarco <pere.marcogarcia@gmail.com>
2021-02-19 18:29:55 +0100

    index

--- src/input.py
+++ src/input.py
@@ -4,4 +4,4 @@ import pandas
 if __name__ == "__main__":
     dataset = pandas.read_csv(filepath_or_buffer = sys.argv[1], sep = '|')
     dataset = dataset.drop_duplicates()
-    dataset.to_csv(path_or_buf= sys.argv[1].replace('.txt', '') + ".csv", sep='|')
+    dataset.to_csv(index = False, path_or_buf= sys.argv[1].replace('.txt', '') + ".csv", sep='|')
