19732d214702adafbb0bbe891cb3ac198064e9d0
Mario Campos Mocholí <mario.identation@gmail.com>
2021-02-19 18:31:24 +0100

    Update input.py

--- src/input.py
+++ src/input.py
@@ -3,5 +3,5 @@ import pandas
 
 if __name__ == "__main__":
     dataset = pandas.read_csv(filepath_or_buffer = sys.argv[1], sep = '|')
-    dataset = dataset.drop_duplicates()
+    dataset.drop_duplicates(inplace=True)
     dataset.to_csv(index = False, path_or_buf= sys.argv[1].replace('.txt', '') + ".csv", sep='|')
