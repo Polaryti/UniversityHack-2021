49ac2f12a4ad3e4701df46a8b41e6a326f9ed3dd
Mario Campos Mocholí <mario.identation@gmail.com>
2021-02-19 20:13:55 +0100

    Update input.py

--- src/input.py
+++ src/input.py
@@ -3,22 +3,32 @@ import math
 import pandas as pd
 
 if __name__ == "__main__":
-    df = pd.read_csv(filepath_or_buffer = sys.argv[1], sep = '|')
+    df = pd.read_csv(filepath_or_buffer=sys.argv[1], sep='|')
     df.drop_duplicates(inplace=True)
     # Quitar la hora de 'fecha'
     df['fecha'] = df['fecha'].apply(lambda x: x.replace(' 0:00:00', ''))
-    # Separación de fecha en 3 columnas
+
+    # Separación de 'fecha' en 3 columnas
     df['fecha'] = pd.to_datetime(df['fecha'])
     df['dia'] = pd.DatetimeIndex(df['fecha']).day
     df['mes'] = pd.DatetimeIndex(df['fecha']).month
     df['anyo'] = pd.DatetimeIndex(df['fecha']).year
     df.drop('fecha', axis=1, inplace=True)
-    # Corregir valores vacios categoria_dos
-    df['categoria_dos'] = df['categoria_dos'].apply(lambda x: 0 if math.isnan(x) else x)
+
+    # Corregir valores vacios de 'categoria_dos'
+    df['categoria_dos'] = df['categoria_dos'].apply(
+        lambda x: 0 if math.isnan(x) else x)
+
     # One-hot encoding de 'estado'
-    df = pd.concat([df, pd.get_dummies(pd.get_dummies(df['estado'], prefix='estado'))], axis=1).drop(['estado'],axis=1)
+    df = pd.concat([df, pd.get_dummies(pd.get_dummies(
+        df['estado'], prefix='estado'))], axis=1).drop(['estado'], axis=1)
+
     # One-hot encoding de 'categoria_uno'
-    df = pd.concat([df, pd.get_dummies(pd.get_dummies(df['categoria_uno'], prefix='categoria_uno'))], axis=1).drop(['categoria_uno'],axis=1)
+    df = pd.concat([df, pd.get_dummies(pd.get_dummies(
+        df['categoria_uno'], prefix='categoria_uno'))], axis=1).drop(['categoria_uno'], axis=1)
+
     # One-hot encoding de 'dia_atipico'
-    df = pd.concat([df, pd.get_dummies(pd.get_dummies(df['dia_atipico'], prefix='dia_atipico'))], axis=1).drop(['dia_atipico'],axis=1)
-    df.to_csv(index = False, path_or_buf= sys.argv[1].replace('.txt', '') + ".csv", sep='|')
+    df = pd.concat([df, pd.get_dummies(pd.get_dummies(
+        df['dia_atipico'], prefix='dia_atipico'))], axis=1).drop(['dia_atipico'], axis=1)
+    df.to_csv(index=False, path_or_buf=sys.argv[1].replace(
+        '.txt', '') + ".csv", sep='|')
