56e648425bbe5d2d3c190e7c3b596f75213aa585
Mario Campos Mocholí <mario.identation@gmail.com>
2021-02-19 18:42:34 +0100

    Update input.py

--- src/input.py
+++ src/input.py
@@ -2,6 +2,7 @@ import sys
 import pandas
 
 if __name__ == "__main__":
-    dataset = pandas.read_csv(filepath_or_buffer = sys.argv[1], sep = '|')
-    dataset.drop_duplicates(inplace=True)
-    dataset.to_csv(index = False, path_or_buf= sys.argv[1].replace('.txt', '') + ".csv", sep='|')
+    df = pandas.read_csv(filepath_or_buffer = sys.argv[1], sep = '|')
+    df.drop_duplicates(inplace=True)
+    df['fecha'] = df['fecha'].apply(lambda x: x.replace(' 0:00:00', ''))
+    df.to_csv(index = False, path_or_buf= sys.argv[1].replace('.txt', '') + ".csv", sep='|')
