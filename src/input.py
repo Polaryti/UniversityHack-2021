926c5d7f47a640ad043cb125ee32dc39ecdbd3fe
Mario Campos Mocholí <mario.identation@gmail.com>
2021-02-19 18:06:00 +0100

    Input

---
+++ src/input.py
@@ -0,0 +1,7 @@
+import sys
+import pandas
+
+if __name__ == "__main__":
+    dataset = pandas.read_csv(filepath_or_buffer = sys.argv[1], sep = '|')
+    dataset = dataset.drop_duplicates()
+    dataset.to_csv(path_or_buf= sys.argv[1].replace('.txt', '') + ".csv", sep='|')
