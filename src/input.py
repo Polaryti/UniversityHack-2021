3b4cddf8c9856da381ebc95654311f7aa37bc7bd
PereMarco <pere.marcogarcia@gmail.com>
2021-02-19 19:00:21 +0100

    Ahora

--- src/input.py
+++ src/input.py
@@ -1,5 +1,5 @@
 import sys
-import panda as pd
+import pandas as pd
 
 if __name__ == "__main__":
     df = pd.read_csv(filepath_or_buffer = sys.argv[1], sep = '|')
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     # Quitar la hora de 'fecha'
     df['fecha'] = df['fecha'].apply(lambda x: x.replace(' 0:00:00', ''))
     # One-hot encoding de 'estado'
-    df = pds.concat([df, pds.get_dummies(pds.get_dummies(df['estado'], prefix='estado'))], axis=1).drop(['estado'],axis=1)
+    df = pd.concat([df, pd.get_dummies(pd.get_dummies(df['estado'], prefix='estado'))], axis=1).drop(['estado'],axis=1)
     df.to_csv(index = False, path_or_buf= sys.argv[1].replace('.txt', '') + ".csv", sep='|')
