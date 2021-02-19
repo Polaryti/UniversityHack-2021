2a88e0a65ed16d9e693da18098502d7493e6cdc4
PereMarco <pere.marcogarcia@gmail.com>
2021-02-19 20:44:40 +0100

    Corrección de precios no establecidos

--- src/input.py
+++ src/input.py
@@ -2,11 +2,39 @@ import sys
 import math
 import pandas as pd
 
+# Diccionarios auxiliaries para la corrección de los precios
+precio_actual = {}
+primer_precio = {}
+
+# Establece los precios no especificados como el último valor indicado para el mismo id ('-1' si nunca se ha especificado)
+def completar_precios(row):
+    precio = float(row['precio'].replace(',','.')) if isinstance(row['precio'], str) else row['precio']
+    identificador = str(row['id'])
+    if math.isnan(precio):
+        precio = precio_actual.get(identificador, -1.0)
+    else:
+        precio_actual[identificador] = precio
+        if primer_precio.get(identificador) == None:
+            primer_precio[identificador] = precio
+    return precio
+
+# Establece los precios que quedan sin especificar como el precio indicado más cercano
+def completar_primeros_precios(row):
+    precio = row['precio']
+    if precio == -1.0:        
+        identificador = str(row['id'])
+        precio = primer_precio.get(identificador)
+        print(precio)
+    return precio
+
 if __name__ == "__main__":
     df = pd.read_csv(filepath_or_buffer = sys.argv[1], sep = '|')
     df.drop_duplicates(inplace=True)
     # Quitar la hora de 'fecha'
     df['fecha'] = df['fecha'].apply(lambda x: x.replace(' 0:00:00', ''))
+    # Completar parámetro precio
+    df['precio'] = df.apply(completar_precios, axis = 1)
+    df['precio'] = df.apply(completar_primeros_precios, axis = 1)
     # Separación de fecha en 3 columnas
     df['fecha'] = pd.to_datetime(df['fecha'])
     df['dia'] = pd.DatetimeIndex(df['fecha']).day
