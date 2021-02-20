98d7250e9f79dcabd0b8a4ae829c91586f769097
Mario Campos Mocholí <mario.identation@gmail.com>
2021-02-20 10:33:35 +0100

    Update train.py

--- src/train.py
+++ src/train.py
@@ -4,14 +4,27 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import sys
 
+def casos_favorables(test, pred):
+    rotura = 0
+    for t, p in test, pred:
+        if p < t:
+            rotura += 1
+
+    return rotura / len(test)
+
+
 if __name__ == "__main__":
     df = pd.read_csv(filepath_or_buffer=sys.argv[1], sep='|')
     X_train, X_test, y_train, y_test = train_test_split(
         df.loc[:, df.columns != 'unidades_vendidas'], df['unidades_vendidas'], test_size=0.35)
-    reg = linear_model.LinearRegression()
+    reg = linear_model.LinearRegression(positive=True)
     reg.fit(X_train, y_train)
     pred = reg.predict(X_test)
-
+    
     print(pred[:10])
     print(y_test[:10])
-    print(mean_squared_error(y_test, pred))
+
+
+    rrmse = mean_squared_error(y_test, pred) / y_train.mean()
+    cf = casos_favorables(y_test, pred)
+    metric = (0.7 * rrmse) + (0.3 + (1 - cf))
