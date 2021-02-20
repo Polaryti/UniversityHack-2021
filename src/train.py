d9cd45866c368c3d994359d4501cc719ca606add
Mario Campos Mocholí <mario.identation@gmail.com>
2021-02-20 10:09:08 +0100

    Update train.py

--- src/train.py
+++ src/train.py
@@ -6,9 +6,11 @@ import sys
 
 if __name__ == "__main__":
     df = pd.read_csv(filepath_or_buffer=sys.argv[1], sep='|')
-    X_train, X_test, y_train, y_test = train_test_split(df.loc[:, df.columns != 'precio'], df['precio'], test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(df.loc[:, df.columns != 'precio'], df['precio'], test_size=0.35)
     reg = linear_model.LinearRegression()
     reg.fit(X_train, y_train)
     pred = reg.predict(X_test)
 
+    print(pred[:10])
+    print(y_test[:10])
     print(mean_squared_error(y_test, pred))
