202ab9e2f699686b9106764e526c7bd3309cc275
Mario Campos Mocholí <mario.identation@gmail.com>
2021-02-20 11:04:33 +0100

    Update train.py

--- src/train.py
+++ src/train.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sklearn import linear_model
+from sklearn import linear_model, svm
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import math
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     df = pd.read_csv(filepath_or_buffer=sys.argv[1], sep='|')
     X_train, X_test, y_train, y_test = train_test_split(
         df.loc[:, df.columns != 'unidades_vendidas'], df['unidades_vendidas'], test_size=0.35)
-    reg = linear_model.LinearRegression()
+    reg = svm.SVR()
     reg.fit(X_train, y_train)
     pred = reg.predict(X_test)
 
