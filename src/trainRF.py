334c9a2337163a33824edc22a799d4ce772e18a3
Mario Campos Mocholí <mario.identation@gmail.com>
2021-02-21 13:10:08 +0100

    Update trainRF.py

--- src/trainRF.py
+++ src/trainRF.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 import math
 import sys
 
@@ -24,13 +24,17 @@ if __name__ == "__main__":
     reg.fit(X_train, y_train)
     pred = reg.predict(X_test)
 
+    pred = map(lambda x : math.ceil(x), pred)
+
     print('{:<24}   {}'.format("Pred", "True"))
-    for i in range(10):
+    for i in range(15):
         print('{:<24}   {}'.format(pred[i], y_test.values[i]))
 
     rrmse = math.sqrt(mean_squared_error(y_test, pred)) / y_train.mean()
     cf = casos_favorables(y_test.values, pred)
     metric = (0.7 * rrmse) + (0.3 * (1 - cf))
 
+    print('El mse: {}'.format(mean_squared_error(y_test, pred)))
+    print('El mae: {}'.format(mean_absolute_error(y_test, pred)))
     print('El cf es: {}'.format(cf))
-    print('La métrica es: {}'.format(metric))
+    print('La métrica propia es: {}'.format(metric))
