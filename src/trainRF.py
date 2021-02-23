b7b436349272b5eee6fbfa71bde67cd53b61afeb
PereMarco <pere.marcogarcia@gmail.com>
2021-02-23 18:53:15 +0100

    trainRF

--- src/trainRF.py
+++ src/trainRF.py
@@ -2,9 +2,9 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error
-from sklearn.model_selection import GridSearchCV
 import math
 import sys
+import numpy as np
 
 
 def casos_favorables(test, pred):
@@ -16,37 +16,37 @@ def casos_favorables(test, pred):
 
     return (total - rotura) / total
 
-def datathon_metric(pred, y_test):
-    rrmse = math.sqrt(mean_squared_error(y_test, pred)) / y_train.mean()
-    cf = casos_favorables(y_test.values, pred)
-    return (0.7 * rrmse) + (0.3 * (1 - cf))
-
 
 if __name__ == "__main__":
     df = pd.read_csv(filepath_or_buffer=sys.argv[1], sep='|')
     X_train, X_test, y_train, y_test = train_test_split(
         df.loc[:, df.columns != 'unidades_vendidas'], df['unidades_vendidas'], test_size=0.3)
+    for i in [100,110,120,130,140,150,160,170,180,190,200]:
+        reg = RandomForestRegressor(verbose=0, n_jobs=-1,n_estimators=i)
+        reg.fit(X_train, y_train)
+        pred = reg.predict(X_test)
+        '''
+        importances = reg.feature_importances_
+        std = np.std([tree.feature_importances_ for tree in reg.estimators_],
+                    axis=0)
+        indices = np.argsort(importances)[::-1]
 
-    parameters = {'n_estimators': (100, 200, 300), 'criterion': ('mae', 'mse'), 'bootstrap': (
-        True, False), 'oob_score': (True, False), 'warm_start': (True, False), 'verbose':[1], 'n_jobs':[-1]}
-    reg = RandomForestRegressor()
-    clf = GridSearchCV(reg, parameters, scoring=mean_absolute_error)
-    clf.fit(X_train, y_train)
+        # Print the feature ranking
+        print("Feature ranking:")
 
-    print(clf.cv_results_.keys())
-
-    pred = clf.predict(X_test)
-
-    pred = list(map(lambda x: round(x), pred))
-    print('{:<24}   {}'.format("Pred", "True"))
-    for i in range(15):
-        print('{:<24}   {}'.format(pred[i], y_test.values[i]))
-
-    rrmse = math.sqrt(mean_squared_error(y_test, pred)) / y_train.mean()
-    cf = casos_favorables(y_test.values, pred)
-    metric = (0.7 * rrmse) + (0.3 * (1 - cf))
-
-    print('El mse: {}'.format(mean_squared_error(y_test, pred)))
-    print('El mae: {}'.format(mean_absolute_error(y_test, pred)))
-    print('El cf es: {}'.format(cf))
-    print('La métrica propia es: {}'.format(metric))
+        for f in range(X_train.shape[1]):
+            print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+        pred = list(map(lambda x : round(x), pred))
+        print('{:<24}   {}'.format("Pred", "True"))
+        for i in range(15):
+            print('{:<24}   {}'.format(pred[i], y_test.values[i]))
+        '''
+        rrmse = math.sqrt(mean_squared_error(y_test, pred)) / y_train.mean()
+        cf = casos_favorables(y_test.values, pred)
+        metric = (0.7 * rrmse) + (0.3 * (1 - cf))
+        '''
+        print('El mse: {}'.format(mean_squared_error(y_test, pred)))
+        print('El mae: {}'.format(mean_absolute_error(y_test, pred)))'''
+        print(i)
+        print('El cf es: {}'.format(cf))
+        print('La métrica propia es: {}'.format(metric))
