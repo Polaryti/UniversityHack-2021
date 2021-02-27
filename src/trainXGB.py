5a1677100a9ba3328906d79bf4b240817bede27d
Mario Campos Mocholí <mario.identation@gmail.com>
2021-02-27 19:25:29 +0100

    EDA (Exploratory Data Analysis) and minor changes
    Modified exploration.py to allow EDA with seaborn.
    Modified trainXGB.py adding standard scaler preprocessing.
    Modified trainRF.py to make iterator more ellegant.

--- src/trainXGB.py
+++ src/trainXGB.py
@@ -2,31 +2,42 @@ import pandas as pd
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.preprocessing import StandardScaler
 import sys
 import math
 
-def casos_favorables(test, pred):
-        rotura = 0
-        total = len(test)
-        for i in range(total):
-            if pred[i] < test[i]:
-                rotura += 1
 
-        return (total - rotura) / total
+def casos_favorables(test, pred):
+    rotura = 0
+    total = len(test)
+    for i in range(total):
+        if pred[i] < test[i]:
+            rotura += 1
+
+    return (total - rotura) / total
+
 
 def datathon_metric(pred, y_test):
     rrmse = math.sqrt(mean_squared_error(y_test, pred)) / y_train.mean()
     cf = casos_favorables(y_test.values, pred)
     return (0.7 * rrmse) + (0.3 * (1 - cf))
 
+
 if __name__ == "__main__":
     df = pd.read_csv(filepath_or_buffer=sys.argv[1], sep='|')
     X_train, X_test, y_train, y_test = train_test_split(
         df.loc[:, df.columns != 'unidades_vendidas'], df['unidades_vendidas'], test_size=0.25)
-    dtrain = xgb.DMatrix(X_train, y_train)
-    dtest = xgb.DMatrix(X_test, y_test) 
 
-    param = {'objective':'reg:tweedie', 'eta':0.4, 'gamma':0.1}
+    # Preprocesamiento
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    dtrain = xgb.DMatrix(X_train, y_train)
+    dtest = xgb.DMatrix(X_test, y_test)
+
+    param = {'objective': 'reg:tweedie'}
     model = xgb.train(param, dtrain)
     pred = model.predict(dtest)
     pred = list(map(lambda x: round(x), pred))
