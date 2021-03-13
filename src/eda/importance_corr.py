34ad052f423483dc90f5427ea80df82ac5f14724
Mario Campos Mocholí <mario.camposmocholi@gmail.com>
2021-03-13 17:25:26 +0100

    Create importance_corr.py

---
+++ src/eda/importance_corr.py
@@ -0,0 +1,69 @@
+from collections import defaultdict
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import spearmanr
+from scipy.cluster import hierarchy
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.inspection import permutation_importance
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import sys
+
+df = pd.read_csv(filepath_or_buffer=sys.argv[1], sep='|')
+X_train, X_test, y_train, y_test = train_test_split(
+    df.loc[:, df.columns != 'unidades_vendidas'], df['unidades_vendidas'], test_size=0.25)
+
+clf = RandomForestRegressor(n_estimators=100, random_state=42)
+clf.fit(X_train, y_train)
+print("Accuracy on test data: {:.2f}".format(clf.score(X_test, y_test)))
+
+result = permutation_importance(clf, X_train, y_train, n_repeats=10,
+                                random_state=42)
+perm_sorted_idx = result.importances_mean.argsort()
+
+tree_importance_sorted_idx = np.argsort(clf.feature_importances_)
+tree_indices = np.arange(0, len(clf.feature_importances_)) + 0.5
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 8))
+ax1.barh(tree_indices,
+         clf.feature_importances_[tree_importance_sorted_idx], height=0.7)
+ax1.set_yticklabels(df.feature_names[tree_importance_sorted_idx])
+ax1.set_yticks(tree_indices)
+ax1.set_ylim((0, len(clf.feature_importances_)))
+ax2.boxplot(result.importances[perm_sorted_idx].T, vert=False,
+            labels=df.feature_names[perm_sorted_idx])
+fig.tight_layout()
+plt.show()
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 8))
+corr = spearmanr(df.loc[:, df.columns != 'unidades_vendidas']).correlation
+corr_linkage = hierarchy.ward(corr)
+dendro = hierarchy.dendrogram(
+    corr_linkage, labels=df.feature_names.tolist(), ax=ax1, leaf_rotation=90
+)
+dendro_idx = np.arange(0, len(dendro['ivl']))
+
+ax2.imshow(corr[dendro['leaves'], :][:, dendro['leaves']])
+ax2.set_xticks(dendro_idx)
+ax2.set_yticks(dendro_idx)
+ax2.set_xticklabels(dendro['ivl'], rotation='vertical')
+ax2.set_yticklabels(dendro['ivl'])
+fig.tight_layout()
+plt.show()
+
+cluster_ids = hierarchy.fcluster(corr_linkage, 1, criterion='distance')
+cluster_id_to_feature_ids = defaultdict(list)
+for idx, cluster_id in enumerate(cluster_ids):
+    cluster_id_to_feature_ids[cluster_id].append(idx)
+selected_features = [v[0] for v in cluster_id_to_feature_ids.values()]
+
+X_train_sel = X_train[:, selected_features]
+X_test_sel = X_test[:, selected_features]
+
+clf_sel = RandomForestRegressor(n_estimators=100, random_state=42)
+clf_sel.fit(X_train_sel, y_train)
+print("Accuracy on test data with features removed: {:.2f}".format(
+      clf_sel.score(X_test_sel, y_test)))
